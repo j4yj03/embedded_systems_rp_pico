b07074ac7bdaedcab5e35aff934d454802b99166
Sidney Göhler <crazy_gosu_sidney@hotmail.de>
2023-09-23 02:43:22 +0200

    schick

--- embedded/vga/vga.c
+++ embedded/vga/vga.c
@@ -15,7 +15,7 @@ int chess[2] = {0x0, 0xFF};
 
 //unsigned int old_frame = 0;
 
-int temp_index, hysnc_count, line_counter, frame_counter, color, color_bit_depth_index, px_idx, colorline_idx = 0;
+int  hysnc_count, line_counter, frame_counter, color, color_bit_depth_index, px_idx, colorline_idx, tmp_idx = 0;
 
 int color_param_1 = 10;
 
@@ -48,7 +48,7 @@ int last_visible_col = 3900; // 98% (32µs TOP - 0.640µs FP)
 void generate_line(int n_bins)
 {
 
-    // each bin consists of 320 / width pixel
+    // each bin consists of FRAME_WIDTH / n_bins pixel
     int bin_size = FRAME_WIDTH / n_bins;
     int bin, start_idx, end_idx, color_idx = 0;
     
@@ -90,7 +90,7 @@ int main()
 
     configure_pwm_irq();    // pwm interrupt handler config + start
 
-    generate_line(12);    // calculate pixel array
+    generate_line(12);    // calculate pixel array with 12 bins
 
     //configure_dma_px();
     sem_init(&display_on, 0, 1);
@@ -119,8 +119,8 @@ int main()
             case 0: // horizontale Streifen
                     while(hsync_get_counter() < last_visible_col)
                     {
-                        temp_index = (int) (line_counter / (64)); 
-                        color =  bits[temp_index % 8]; 
+                        tmp_idx = (int) (line_counter / 64); 
+                        color =  bits[tmp_idx % 8]; 
                         gpio_put_masked(color_bit_mask, (color << COLOR_GPIO_OFFSET));
                     };
                     break;
@@ -128,8 +128,8 @@ int main()
             case 1: // vertikale Streifen
                     while(hsync_get_counter() < last_visible_col)
                     {
-                        temp_index = (int) ((hsync_get_counter()) / (256));
-                        gpio_put_masked(color_bit_mask, (bits[temp_index % 8] << COLOR_GPIO_OFFSET));
+                        tmp_idx = (int) (hsync_get_counter() / 256);
+                        gpio_put_masked(color_bit_mask, (bits[tmp_idx % 8] << COLOR_GPIO_OFFSET));
 
                     };
                     break;
@@ -154,8 +154,8 @@ int main()
             case 3: // zur Laufzeit berechnetes Schachbrett
                     while(hsync_get_counter() < last_visible_col)
                     {
-                        temp_index = (int) (line_counter / (24)) ^ ((hsync_get_counter()) / 4);                       
-                        gpio_put_masked(color_bit_mask, (chess[temp_index % 2] << COLOR_GPIO_OFFSET));
+                        tmp_idx = (int) (line_counter / 24) ^ (hsync_get_counter() / 4);                       
+                        gpio_put_masked(color_bit_mask, (chess[tmp_idx % 2] << COLOR_GPIO_OFFSET));
                 
                     };
                     break;
